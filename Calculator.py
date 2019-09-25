e4676557c8937a234e74a07a77b1f9de6538484c
Md. Nur Amin Sifat <41265694+NurAminSifatTanha@users.noreply.github.com>
2019-09-25 19:46:36 +0600

    basic calculator using OOP concept

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+class Calculator:
+	def __init__(self,a,b):
+    	self.a=a
+    	self.b=b
+	def addition(self):
+    	print(self.a+self.b)
+	def sub(self):
+    	print(self.a-self.b)
+	def mul(self):
+    	print(self.a*self.b)
+	def div(self):
+    	print(float(self.a/self.b))
+print("Enter choice: ")
+n=int(input())
+if n==1:
+	c=Calculator(int(input("Enter a: ")),int(input("Enter b: ")))
+	c.addition()
+elif n==2:
+	c=Calculator(int(input("Enter a: ")),int(input("Enter b: ")))
+	c.sub()
+elif n==3:
+	c=Calculator(int(input("Enter a: ")),int(input("Enter b: ")))
+	c.mul()
+elif n==4:
+	try:
+    	c=Calculator(int(input("Enter a: ")),int(input("Enter b: ")))
+    	c.div()
+	except:
+    	print("Divided by Zero")
+else:
+	print("Operation not possible")
